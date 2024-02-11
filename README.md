768e814d2c18b9ba65ef379394e51baaef33c43f
royaljacquesytb <63215754+royaljacques@users.noreply.github.com>
2024-02-12 00:30:25 +0100

    upload full plugin

---
+++ README.md
@@ -0,0 +1,21 @@
+## CustomFurnaceRecipes 
+
+<p>This plugin allows you to save several new recipes for your oven. imagine a countless number of cooking possibilities for all your items!!</p>
+<p>For the moment, nothing has been done to delete an already existing recipe. Please wait a few days until a solution is found.</p>
+
+### config
+```yaml
+recipes:
+  0:
+    input: "minecraft:coal"
+    output: "minecraft:iron_ingot"
+  1:
+    input: "minecraft:cobblestone"
+    output: "minecraft:coal"
+```
+
+### images
+
+<center>
+<img src="./assets/exemple.png" width="50%" height="50%">
+</center>
---
+++ src/royal/furnacerecipes/Main.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace royal\furnacerecipes;
+
+use pocketmine\crafting\ExactRecipeIngredient;
+use pocketmine\crafting\FurnaceRecipe;
+use pocketmine\crafting\FurnaceType;
+use pocketmine\event\Listener;
+use pocketmine\item\Item;
+use pocketmine\item\StringToItemParser;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+
+class Main extends PluginBase implements Listener
+{
+    protected function onEnable(): void
+    {
+        $this->saveDefaultConfig();
+
+        $recipes = $this->getConfig()->get("recipes");
+        foreach ($recipes as $recipe){
+            $input = StringToItemParser::getInstance()->parse($recipe["input"]);
+            $output = StringToItemParser::getInstance()->parse($recipe["output"]);
+            $this->registerFurnace($input, $output);
+        }
+    }
+    public function registerFurnace(Item $input, Item $output): void {
+        $furnaceRecipe = new FurnaceRecipe(
+            $output,
+            new ExactRecipeIngredient($input)
+        );
+        $manager = $this->getServer()->getCraftingManager()->getFurnaceRecipeManager(FurnaceType::FURNACE());
+        $manager->register($furnaceRecipe);
+    }
+}
